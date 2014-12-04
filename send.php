84fa0ab0b88d3345c201d15436c6e46b5e8765c9
GuillaumeHaben <pyxouze@gmail.com>
2014-12-04 20:19:27 +0100

    Ajout du Drag n Drop

---
+++ send.php
@@ -0,0 +1,41 @@
+<?php
+
+function make_thumb($src, $dest, $desired_width) {
+
+	/* read the source image */
+	$source_image = imagecreatefromjpeg($src);
+	$width = imagesx($source_image);
+	$height = imagesy($source_image);
+	
+	/* find the "desired height" of this thumbnail, relative to the desired width  */
+	$desired_height = floor($height * ($desired_width / $width));
+	
+	/* create a new, "virtual" image */
+	$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+	
+	/* copy source image at a resized size */
+	imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+	
+	/* create the physical thumbnail image to its destination */
+	imagejpeg($virtual_image, $dest);
+}
+
+$ds          = DIRECTORY_SEPARATOR;
+ 
+$storeFolder = 'img/works/photos';
+ 
+if (!empty($_FILES)) {
+     
+    $tempFile = $_FILES['file']['tmp_name'];   
+      
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
+     
+    $targetFile =  $targetPath. $_FILES['file']['name'];
+ 
+    move_uploaded_file($tempFile,$targetFile);
+	
+	make_thumb($targetFile, $targetFile . ".jpg", 263);
+     
+}
+
+?>
---
+++ upload.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="La Nuit de l'Info - Telecom Nancy">
+    <meta name="author" content="Guillaume HABEN">
+
+    <title>Nuit de l'Info 2014</title>
+	
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/animations.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+	<link href="css/dropzone.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	
+    <!-- Navigation -->
+    <div id="navigation">
+        <nav class="navbar navbar-custom" role="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="site-logo">
+                            <a href="index.html" class="brand">Photos Upload</a>
+                        </div>
+                    </div>
+                          
+
+                    <div class="col-md-6">
+	                    <!-- Brand and toggle get grouped for better mobile display -->
+	                    <div class="navbar-header">
+	                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+	                            <i class="fa fa-bars"></i>
+	                        </button>
+	                    </div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="menu">
+						    <ul class="nav navbar-nav navbar-right">
+						          <li class="active"><a href="index.html">Accueil</a></li>
+						    </ul>
+						</div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+<!-- Section: services -->
+    <section id="service" class="home-section color-dark bg-gray">
+		<div class="container marginbot-50">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="animatedParent">
+					<div class="section-heading text-center">
+					<h2 class="h-bold">Drag n Drop</h2>
+					<div class="divider-header"></div>
+					</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="text-center">
+		<div class="container">
+			<form action="send.php" id="myAwesomeDropzone" class="dropzone"></form><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+		</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<ul class="footer-menu">
+						<li><a href="#">Accueil</a></li>
+					</ul>
+				</div>
+				<div class="col-md-6 text-right">
+					<p>&copy; Copyright 2014 - <font color="#f18b26">Telecom Nancy</font></p>
+				</div>
+			</div>	
+		</div>
+	</footer>
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>	 
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.easing.min.js"></script>	
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.appear.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="js/nivo-lightbox.min.js"></script>
+	<script src="js/dropzone.js"></script>
+	<script type="text/javascript">
+	Dropzone.options.myAwesomeDropzone = {
+		createImageThumbnails: false,
+	};
+	</script>
+    <script src="js/custom.js"></script>
+	<script src="js/css3-animate-it.js"></script>
+
+</body>
+
+</html>
